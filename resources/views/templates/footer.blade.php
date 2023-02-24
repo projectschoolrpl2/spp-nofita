faab4f6fc0b8ce4972cfa071a2e7c943548fc7e5
Nofita <projectschoolrpl2@gmail.com>
2023-02-24 17:16:33 +0700

    templating

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,25 @@
+
+<div class="footer">
+    <div class="copyright">
+        <p>Copyright © Designed by <a href="#" target="_blank">Nofita Lulu</a> 2023</p>
+    </div>
+</div>
+
+<!-- Required vendors -->
+<script src="{{asset ('projek') }}/vendor/global/global.min.js"></script>
+<script src="{{asset ('projek') }}/js/quixnav-init.js"></script>
+<script src="{{asset ('projek') }}/js/custom.min.js"></script>
+
+<script src="{{asset ('projek') }}/vendor/chartist/js/chartist.min.js"></script>
+
+<script src="{{asset ('projek') }}/vendor/moment/moment.min.js"></script>
+<script src="{{asset ('projek') }}/vendor/pg-calendar/js/pignose.calendar.min.js"></script>
+
+
+<script src="{{asset ('projek') }}/js/dashboard/dashboard-2.js"></script>
+@stack('js')
+<!-- Circle progress -->
+
+</body>
+
+</html>
---
+++ resources/views/templates/layout.blade.php
@@ -0,0 +1,140 @@
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Aplikasi Pembayaran SPP</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset ('projek') }}/images/favicon.png">
+    <link href="{{asset ('projek') }}/vendor/pg-calendar/css/pignose.calendar.min.css" rel="stylesheet">
+    <link href="{{asset ('projek') }}/vendor/chartist/css/chartist.min.css" rel="stylesheet">
+    <link href="{{asset ('projek') }}/css/style.css" rel="stylesheet">
+    @stack('css')
+
+</head>
+
+<body>
+
+    <div id="preloader">
+        <div class="sk-three-bounce">
+            <div class="sk-child sk-bounce1"></div>
+            <div class="sk-child sk-bounce2"></div>
+            <div class="sk-child sk-bounce3"></div>
+        </div>
+    </div>
+   
+    <div id="main-wrapper">
+
+        <div class="nav-header">
+            <a href="#" class="brand-logo">
+                <img class="logo-abbr" src="{{asset ('projek') }}/images/logo.png" alt="">
+                <img class="logo-compact" src="{{asset ('projek') }}/images/logo-text.png" alt="">
+                <span class="brand-title">Pembayaran SPP</span>
+            </a>
+
+            <div class="nav-control">
+                <div class="hamburger">
+                    <span class="line"></span><span class="line"></span><span class="line"></span>
+                </div>
+            </div>
+        </div>
+        
+        <div class="header">
+            <div class="header-content">
+                <nav class="navbar navbar-expand">
+                    <div class="collapse navbar-collapse justify-content-between">
+                        <div class="header-left">
+                            <div class="search_bar dropdown">
+                                <span class="search_icon p-3 c-pointer" data-toggle="dropdown">
+                                    <i class="mdi mdi-magnify"></i>
+                                </span>
+                                <div class="dropdown-menu p-0 m-0">
+                                    <form>
+                                        <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                        <ul class="navbar-nav header-right">
+                            <li class="nav-item dropdown notification_dropdown">
+                                <a class="nav-link" href="#" role="button" data-toggle="dropdown">
+                                    <i class="mdi mdi-bell"></i>
+                                    <div class="pulse-css"></div>
+                                </a>
+                                
+                            </li>
+                            <li class="nav-item dropdown header-profile">
+                                <a class="nav-link" href="#" role="button" data-toggle="dropdown">
+                                    <i class="mdi mdi-account"></i>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <a href="#" class="dropdown-item">
+                                        <i class="icon-user"></i>
+                                        <span class="ml-2">Profile </span>
+                                    </a>
+                                    <a href="#" class="dropdown-item">
+                                        <i class="icon-key"></i>
+                                        <span class="ml-2">Logout </span>
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        
+        <div class="quixnav">
+            <div class="quixnav-scroll">
+                <ul class="metismenu" id="menu">
+                    <li class="nav-label first">Main Menu</li>
+                    
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <i class="icon icon-single-04"></i><span class="nav-text">Dashboard</span></a>
+                    </li>
+
+                    <li class="nav-label">Manage Data</li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <i class="fa fa-graduation-cap" aria-hidden="true"></i><span class="nav-text">Students</span></a>
+                    </li>
+
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <i class="fa fa-user-plus" aria-hidden="true"></i><span class="nav-text">Officers</span></a>
+                    </li>
+
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <i class="fa fa-building-o" aria-hidden="true"></i><span class="nav-text">Classes</span></a>
+                    </li>
+
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <i class="fa fa-money" aria-hidden="true"></i><span class="nav-text">SPP</span></a>
+                    </li>
+                    
+                    <li class="nav-label">Transactions</li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <i class="fa fa-credit-card" aria-hidden="true"></i><span class="nav-text">Payments</span></a>
+                    </li>
+
+                    <li class="nav-label">Histories</li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <i class="fa fa-line-chart" aria-hidden="true"></i><span class="nav-text">Payment Histories</span></a>
+                    </li>
+ 
+                    <li class="nav-label">Report</li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <i class="fa fa-print" aria-hidden="true"></i><span class="nav-text">Reports</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+        @yield('main')
+        
+    </div>
+
+@include('templates.footer')
